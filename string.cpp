c8ed260b30d6b850bd4fbff35f56d768130c3354
colorbox <colorbox222@gmail.com>
2019-11-09 20:54:06 +0900

    Add strings

---
+++ string.cpp
@@ -0,0 +1,16 @@
+
+string replace(string target, string finder, string replacement){
+  string result=string(target);
+
+  string::size_type pos=0;
+
+  pos = result.find(finder, pos);
+  while(pos != string::npos){
+    result.replace(pos, finder.length(), replacement);
+    pos = result.find(finder);
+  }
+
+  return result;
+}
+
+
