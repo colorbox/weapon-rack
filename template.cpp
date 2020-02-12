577fcc7b6b10cacd7269c9f3cd52de8e64fc99e2
colorbox <colorbox222@gmail.com>
2020-02-13 00:48:54 +0900

    Add MOD and rrep to template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 
 #define rep(i,n) for(int i = 0; i < (n); i++)
+#define rrep(i,n) for(int i = (n)-1; i >= 0; i--)
 #define ll long long
+
+#define MOD 1000000007
 
 using namespace std;
 
