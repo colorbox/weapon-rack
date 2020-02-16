7a9e8f726477c6c9ecafe2fc83e3a5fe0a60eb72
colorbox <colorbox222@gmail.com>
2020-02-16 17:59:58 +0900

    Add rep1 rrep1
    closes: #2

--- template.cpp
+++ template.cpp
@@ -2,6 +2,9 @@
 
 #define rep(i,n) for(int i = 0; i < (n); i++)
 #define rrep(i,n) for(int i = (n)-1; i >= 0; i--)
+#define rep1(i,n) for(int i = 1; i <= (n); i++)
+#define rrep1(i,n) for(int i = (n); i > 0; i--)
+
 #define ll long long
 
 #define MOD 1000000007
